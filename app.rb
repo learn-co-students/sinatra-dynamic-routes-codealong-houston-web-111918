42ef7c6ccfefd2ca650a6d7c34235966468e7fd1
github username <github email address>
2018-12-12 08:55:47 -0600

    dynamic coding

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -14,5 +15,17 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/goodbye/:name' do
+  @user_name = params[:name]
+  "Goodbye, #{@user_name}."
+  end
+
+  get '/multiply/:num1/:num2' do
+    # #set each instance variable equal to a parameter
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    # #return the product of the two parameters
+    (@num1.to_i * @num2.to_i).to_s
+  end
 
 end
